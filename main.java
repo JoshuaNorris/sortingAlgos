ec9ff12ca095725aa9da48843d5f0682a96a9687
Joshua Norris <joshua.norris52@gmail.com>
2024-03-22 14:06:13 -0500

    Added MergeSort

--- main.java
+++ main.java
@@ -14,11 +14,56 @@ public class main {
 			add(7);
 			add(0);
 		}};
-	    System.out.println(insertion_sort(input));
+	    System.out.println(merge_sort(input));
 	}
 
 // 7 elements
 // [3,35,12,16,12,7,0]
+
+	public static ArrayList<Integer> merge_sort(ArrayList<Integer> nums) {
+		ArrayList<Integer> list = new ArrayList<>(nums.subList(0, nums.size() / 2));
+		return recursive_merge_sort(new ArrayList<>(nums.subList(0, nums.size() / 2)), new ArrayList<>(nums.subList(nums.size() / 2, nums.size())));
+	}
+
+	private static ArrayList<Integer> recursive_merge_sort(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+		if (list1.size() <= 1 && list2.size() <=1) {
+			return combine_merge(list1, list2);
+		} else {
+			return combine_merge(recursive_merge_sort(
+				new ArrayList<>(list1.subList(0, list1.size() / 2)), new ArrayList<>(list1.subList(list1.size() / 2, list1.size()))
+			), recursive_merge_sort(
+				new ArrayList<>(list2.subList(0, list2.size() / 2)), new ArrayList<>(list2.subList(list2.size() / 2, list2.size()))
+			));
+		}
+
+	}
+
+	private static ArrayList<Integer> combine_merge(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+		// This function assumes the list are ordered
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		boolean finished = false;
+		while (!finished) {
+			if (list1.size() == 0) {
+				result.addAll(list2);
+				finished = true;
+			} else if (list2.size() == 0) {
+				result.addAll(list1);
+				finished = true;
+			} else {
+				int list1_element = list1.get(0);
+				int list2_element = list2.get(0);
+				if (list1_element < list2_element) {
+					int temp = list1.remove(0);
+					result.add(temp);
+				} else {
+					int temp = list2.remove(0);
+					result.add(temp);
+				}
+			}
+
+		}
+		return result;
+	}
 
 	public static ArrayList<Integer> insertion_sort(ArrayList<Integer> nums) {
 		for (int iteration = 1; iteration < nums.size(); iteration ++) {
